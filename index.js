3309d94628dac6f440a8ea0ca505d1d4fdc85397
michalis.almpertis <mike.almpertis@gmail.com>
2019-12-06 00:17:40 +0200

    ADD: dotenv

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const Discord = require('discord.js')
+const { Client } = require('discord.js')
 const fetch = require("node-fetch");
+require('dotenv').config();
 
-const client = new Discord.Client();
-
-const token = 'NjUyMjQxMTUwMTk1MDA3NDg4.XelxJA.ewHn0mtSw59xFIJ-CGnG2h66WUc';
-
-const getWeather = async () => {
-  try {
-    const res = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Athens,gr&APPID=eb789f77e1b5c4e30e37994f87afa668');
-    const data = await res.json();
-    console.log(data);
-    return `The weather in Athens is ${data.weather[0].description} and the temperature is ${(data.main.temp - 272.15).toFixed(2)} °C`
-  } catch {
-    return 'Sorry, I cannot find the weather right now... Gamiesai...'
-  }
-}
-
-client.login(token);
+const client = new Client({
+  disableEveryone: true
+});
 
 client.on('ready', ()=>{
-  console.log('I am ready!')
+  console.log(`Bot ${client.user.username} is ready!`)
+  client.user.setPresence({
+    status: 'online',
+    game: {
+      name: 'Chillin n Killin',
+      type: 'WATCHING'
+    }
+  })
 })
 
-client.on('message', (msg) => {
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+client.on('message', async (msg) => {
   if(msg.author.username !== 'PowderBot') {
     switch(msg.toString()) {
       case 'weather':
@@ -32,3 +29,14 @@ client.on('message', (msg) => {
     }
   }
 })
+
+const getWeather = async () => {
+  try {
+    const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Athens,gr&APPID=${process.env.OPENWEATHER_TOKEN}`);
+    const data = await res.json();
+    console.log(data);
+    return `The weather in Athens is ${data.weather[0].description} and the temperature is ${(data.main.temp - 272.15).toFixed(2)} °C`
+  } catch {
+    return 'Sorry, I cannot find the weather right now... Gamiesai...'
+  }
+}
