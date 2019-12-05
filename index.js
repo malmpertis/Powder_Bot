60ec7503f8d4771889314825f6810d37e4e5a6b8
michalis.almpertis <mike.almpertis@gmail.com>
2019-12-06 01:14:56 +0200

    ADD: prefix, multi city weather and checks

--- index.js
+++ index.js
@@ -18,26 +18,35 @@ client.on('ready', ()=>{
 })
 
 client.on('message', async (msg) => {
-  if(msg.author.username !== 'PowderBot') {
-    switch(msg.toString()) {
-      case 'weather':
-        const response = await getWeather();
-        return msg.reply(response);
-      default:
-        return msg.reply('gamiesai');
-    }
+  const prefix = process.env.BOT_PREFIX;
+
+  if(msg.author.bot) return;
+  if(!msg.guild) return;
+  if(!msg.content.startsWith(prefix)) return;
+
+  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  console.log(args);
+  
+  switch(command) {
+    case 'weather':
+      const response = await getWeather(args[0]);
+      return msg.reply(response);
+    default:
+      return msg.reply('gamiesai');
   }
 })
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
-const getWeather = async () => {
+const getWeather = async (city) => {
+  if(!city) return `Please write a city name`
   try {
-    const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Athens,gr&APPID=${process.env.OPENWEATHER_TOKEN}`);
+    const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city.toLowerCase()},gr&APPID=${process.env.OPENWEATHER_TOKEN}`);
     const data = await res.json();
     console.log(data);
-    return `The weather in Athens is ${data.weather[0].description} and the temperature is ${(data.main.temp - 272.15).toFixed(2)} °C`
+    return `The weather in ${city} is ${data.weather[0].description} and the temperature is ${(data.main.temp - 272.15).toFixed(2)} °C`
   } catch {
-    return 'Sorry, I cannot find the weather right now... Gamiesai...'
+    return `Sorry, I cannot find the weather for ${city} right now... Gamiesai...`
   }
 }
