5dba14be4f7b66bdb2b9800f4f51b7be151872ed
michalis.almpertis <mike.almpertis@gmail.com>
2019-12-05 23:22:56 +0200

    ADD: weather command

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const Discord = require('discord.js')
+const fetch = require("node-fetch");
+
 const client = new Discord.Client();
 
-const token = 'NjUyMjQxMTUwMTk1MDA3NDg4.Xelqyw.zdPgcY5LmeMpDe5VLUOV9wNYUa4';
+const token = 'NjUyMjQxMTUwMTk1MDA3NDg4.XelxJA.ewHn0mtSw59xFIJ-CGnG2h66WUc';
+
+const getWeather = async () => {
+  try {
+    const res = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Athens,gr&APPID=eb789f77e1b5c4e30e37994f87afa668');
+    const data = await res.json();
+    console.log(data);
+    return `The weather in Athens is ${data.weather[0].description} and the temperature is ${(data.main.temp - 272.15).toFixed(2)} °C`
+  } catch {
+    return 'Sorry, I cannot find the weather right now... Gamiesai...'
+  }
+}
+
 client.login(token);
 
 client.on('ready', ()=>{
@@ -9,13 +23,12 @@ client.on('ready', ()=>{
 })
 
 client.on('message', (msg) => {
-  if(msg.author.username!=="PowderBot") {
-    if(msg.content === "ti kaneis?") {
-      console.log('gamw')
-      return msg.reply("gamw ton adriko");
-    } else {
-      console.log('gamietai')
-      return msg.reply("O adrikos gamietai!");
+  if(msg.author.username !== 'PowderBot') {
+    switch(msg.toString()) {
+      case 'weather':
+        getWeather().then(response => msg.reply(response))
+      default:
+        msg.reply('gamiesai');
     }
   }
 })
